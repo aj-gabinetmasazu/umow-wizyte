2197432ba1848443deac97b6c37ee1b64d28a379
Anita <52784073+aticaa@users.noreply.github.com>
2026-09-11 07:59:14 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-window.TERM_CONFIG = {
-  SUPABASE_URL: "https://oiabhlviecdkstuedmdp.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_IoteuBlBG9exrD5ubm-z4A_9szDC9Md",
-  WORKSPACE_ID: "11111111-1111-1111-1111-111111111111"
+window.TERM_CONFIG={
+  SUPABASE_URL:'https://oiabhlviecdkstuedmdp.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY:'sb_publishable_IoteuBlBG9exrD5ubm-z4A_9szDC9Md',
+  WORKSPACE_ID:'11111111-1111-1111-1111-111111111111'
 };
